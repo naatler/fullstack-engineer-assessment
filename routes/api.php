a8bd27b0b79a64b3c79ad58d74fd71541897c236
Nadlar Aditya <nadlaraditya13@gmail.com>
2026-05-29 21:12:22 +0700

    feat: add order API endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('products', ProductController::class)
     ->only(['index', 'store', 'show']);
+
+Route::post('orders', [OrderController::class, 'store']);
+Route::get('orders/{order}', [OrderController::class, 'show']);
